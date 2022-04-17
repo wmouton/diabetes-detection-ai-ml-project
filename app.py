6ea655708192d29c9a67fd06eea790793fe33094
l33th <l33th@protonmail.ch>
2022-04-17 06:19:47 +0200

    title, subtitle, image and caption

---
+++ app.py
@@ -0,0 +1,20 @@
+# This AI & ML project is a diabetes detection program that can detect if someone has diabetes
+
+# Library Imports
+import streamlit as st
+import pandas as pd
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from PIL import Image
+
+# Title And Subtitle
+st.write("""
+# AI & ML Diabetes Detection Web Application
+Detect If Someone Has Diabetes Using This Web Appliaction.
+""")
+
+# Image To Display
+image = Image.open('./img/image.jpg')
+st.image(image, caption='ARTIFICIAL INTELLIGENCE & MACHINE LEARNING', use_column_width=True)
